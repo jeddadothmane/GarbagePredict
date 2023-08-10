6fa4e90d4fd993474f9ec45e5a54d1e752653d3f
jeddadothmane <othmane.jeddad20@gmail.com>
2023-08-10 13:08:29 +0100

    commit

--- app.py
+++ app.py
@@ -35,6 +35,35 @@ def run_yolo_prediction(image_path):
         return result_url
     else:
         return None
+    yolo_model_path = "yolo_model/best.pt"
+    temp_result_dir = os.path.join('static', 'results')
+    yolo_command = f'yolo task=detect mode=predict model={yolo_model_path} project={temp_result_dir} conf=0.25 source={image_path} save=True'
+
+    os.makedirs(temp_result_dir, exist_ok=True)
+
+    try:
+        subprocess.run(yolo_command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        return "Error running YOLO prediction.", None
+
+    result_image_path = os.path.join(temp_result_dir, 'uploaded_image.jpg')
+    predict_dirs = [f for f in os.listdir(temp_result_dir) if f.startswith('predict')]
+    latest_predict_dir = max(predict_dirs, key=lambda x: int(x[len('predict'):]) if x[len('predict'):] else -1, default=None)
+
+    if latest_predict_dir:
+        result_image_path = os.path.join(temp_result_dir, latest_predict_dir, 'uploaded_image.jpg')
+
+        predicted_images_dir = os.path.join('results', latest_predict_dir)
+        os.makedirs(predicted_images_dir, exist_ok=True)
+
+        predicted_image_dest = os.path.join(predicted_images_dir, 'uploaded_image.jpg')
+        shutil.copy(result_image_path, predicted_image_dest)
+
+        result_url = url_for('static', filename=predicted_image_dest.replace("\\", "/"))
+
+        return result_url
+    else:
+        return None
 @app.route('/')
 def index():
     return render_template('index.html')
